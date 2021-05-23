e5845408018815ecf2689c9bedd0436c2a37f027
Jay Morgan <jaymiles17@gmail.com>
2021-05-23 14:06:41 +0100

    add basic highlighting and indentation

---
+++ slurp-mode.el
@@ -0,0 +1,34 @@
+(defconst slurp-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    ;; string deliminter
+    (modify-syntax-entry ?' "\"" table)
+    (modify-syntax-entry ?\" "\"" table)
+    ;; comments
+    (modify-syntax-entry ?\; ". 12" table)
+    (modify-syntax-entry ?\n ">" table)
+    table))
+
+(defconst slurp-mode-pretty-symbols-alist
+  '(("lambda" . ?λ)))
+
+(defconst slurp-mode-font-lock-keywords
+  ;; (regexp-opt '("defparam" "defun" "library" "lambda") t)
+  (list '("\\<\\(def\\(?:param\\|un\\)\\|library\\|lambda\\)\\>" . font-lock-builtin-face)
+        '("\\(\:\\w+\\)" . font-lock-keyword-face)
+        '("\\([0-9.]+\\)" . font-lock-constant-face)
+        '("\\<\\(defun \\w+ \\)\\>" . font-lock-constant-face))
+  "Default highlighting for SluRp mode")
+
+(defun slurp-mode ()
+  "Major mode for SluRp"
+  (interactive)
+  (kill-all-local-variables)
+  (set-syntax-table slurp-mode-syntax-table)
+  (set (make-local-variable 'font-lock-defaults) '(slurp-mode-font-lock-keywords))
+  (set (make-local-variable 'prettify-symbols-alist) slurp-mode-pretty-symbols-alist)
+  (set (make-local-variable 'lisp-body-indent) 1)
+  (set (make-local-variable 'indent-line-function) 'lisp-indent-line))
+
+(add-to-list 'auto-mode-alist '("\\.slurp\\'" . slurp-mode))
+
+(provide 'slurp-mode)
