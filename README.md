96dec9574d580835cb42bf650d68818727d12a9b
Jay Morgan <jaymiles17@gmail.com>
2021-05-26 15:19:24 +0100

    Add slurp repl

--- README.md
+++ README.md
@@ -5,7 +5,15 @@ language](https://github.com/jaypmorgan/slurp).
 
 ## Installation
 
+Below is a basic installation and configuration. The `slurp-model.el` file contains both the major more for slurp scripting, as well as the interface for communicating with the slurp repl.
+
 ```emacs-lisp
-(use-package  slurp-mode
-  :quelpa (slurp-mode :fetcher github :repo "jaypmorgan/slurp-mode"))
+(use-package slurp-mode
+  :load-path "~/path/to/slurp-mode.el"
+  :bind (("C-c C-c" . slurp-repl-send-region)
+         ("C-c C-b" . slurp-repl-send-buffer)
+         ("C-c C-z" . run-slurp))
+  :init
+  (require 'slurp-repl-mode)
+  (setq slurp-repl-location "~/path/to/slurp"))
 ```
--- slurp-mode.el
+++ slurp-mode.el
@@ -45,4 +45,61 @@
 
 (add-to-list 'auto-mode-alist '("\\.slurp\\'" . slurp-mode))
 
+;;; repl
+
+(defvar slurp-repl-location "" "Path to the slurp repl file")
+(defvar slurp-repl-arguments '() "Arguments to pass to slurp upon startup")
+(defvar slurp-repl-prompt-regex "^Slurp> " "Prompt for repl")
+(defvar slurp-repl-mode-map
+  (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
+    map)
+  "Basic mode map for the slurp repl")
+
+(defun slurp-repl--get-buffer-contents ()
+  "Get buffer contents as a string"
+  (buffer-substring-no-properties 1 (point-max)))
+
+(defun slurp-repl--get-region-contents ()
+  "Get the text of highlighted region as string"
+  (buffer-substring-no-properties (region-beginning) (region-end)))
+
+(defun slurp-repl-send-buffer ()
+  "Send entire buffer to the repl"
+  (interactive)
+  (comint-send-string "*SluRp*" (slurp-repl--get-buffer-contents)))
+
+(defun slurp-repl-send-region ()
+  "Send highlighted region to repl"
+  (interactive)
+  (comint-send-string "*SluRp*" (slurp-repl--get-region-contents)))
+
+(defun run-slurp ()
+  "Run an inferior instance of the slurp repl inside Emacs."
+  (interactive)
+  (let* ((slurp-repl slurp-repl-location)
+         (buffer (comint-check-proc "SluRp")))
+    (pop-to-buffer-same-window
+     (if (or buffer (not (derived-mode-p 'slurp-repl-mode))
+             (comint-check-proc (current-buffer)))
+         (get-buffer-create (or buffer "*SluRp*"))
+       (current-buffer)))
+    (unless buffer
+      (apply 'make-comint-in-buffer "SluRp" buffer
+             slurp-repl slurp-repl-arguments)
+      (slurp-repl-mode))))
+
+(defun slurp-repl-mode--initialise ()
+  (setq comint-process-echoes t)
+  (setq comint-use-prompt-regexp t))
+
+(define-derived-mode slurp-repl-mode comint-mode "SluRp"
+  "Major mode for SluRp repl"
+  (setq comint-prompt-regexp slurp-repl-prompt-regex
+        comint-prompt-read-only t)
+  (set (make-local-variable 'paragraph-separate) "\\'")
+  (set (make-local-variable 'paragraph-start) slurp-repl-prompt-regex))
+
+(add-hook 'slurp-repl-mode 'slurp-repl-mode--initialise)
+
+(provide 'slurp-repl-mode)
 (provide 'slurp-mode)
